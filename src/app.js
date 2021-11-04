a27ce6d86db0f5c37db88fd0f8387e6450e6aab3
jalbiston <jacob.albiston@gmail.com>
2021-11-04 16:51:38 -0600

    finished up

--- src/app.js
+++ src/app.js
@@ -2,28 +2,25 @@ const fs = require ("fs")
 const path = require ("path")
 const express = require ("express")
 const app =express()
+const accountRoutes = require('./routes/accounts')
+const servicesRoutes = require('./routes/services')
 
+const { accounts, users, writeJSON } = require('./data')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
 
-const accountData = fs.readFileSync(
-    path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 
-const accounts = JSON.parse(accountData)
 
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8')
-const users = JSON.parse(userData)
 
 app.get('/', (req, res)=> res.render('index', {title: 'Account Summary', accounts}));
+app.use('/account', accountRoutes)
+app.use('/services', servicesRoutes)
 
-app.get('/savings', (req, res) => {res.render('account', {account: accounts.savings})})
-app.get('/checking', (req, res) => {res.render('account', {account: accounts.checking})})
-app.get('/credit', (req, res) => {res.render('account', {account: accounts.credit})})
 app.get('/profile',(req, res) => {res.render('profile', { user: users[0] })})
-
 
 
 app.listen(3000, ()=> console.log('This stupid project is running on port 3000'))
